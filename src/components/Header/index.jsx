248ea6d487a319c8c06fa01e45c55ba85c4630ca
Elisberto Junior <elisberto_jr@hotmail.com>
2023-04-08 23:30:29 -0300

    first commit

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -4,7 +4,7 @@ const Header = () => {
   return (
     <header className={styles.header}>
       <div className="container">
-        <img className={styles.logoImg} src="../../assets/IMC.png" alt="IMC" />
+        <img className={styles.logoImg} src="https://play-lh.googleusercontent.com/PmOnpf2uN_o-0Uoff75f4nppoNNRW3IX7HFEmnRsuSzCWeD3L0iixBtJ9lIAaCWcGA" alt="IMC" />
         <h1>Calculadora de IMC</h1>
       </div>
     </header>
